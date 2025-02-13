2486f3aa27035f42af2c1c6264a66de6a3b921cb
MIG217 <133089678+MIG217@users.noreply.github.com>
2025-02-13 09:27:47 +0000

    update

---
+++ our_first_script.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+2+2
+
+2+3
+
+4**2
+
+6/2
+
+x=2
+
+y=5
+
+z=x+y
+
+# comments in Python start with HASHTAGS!!!
+
+# this line cannot be evaluated!!!
+
+# 2+2
+
+# comments enable us to type reminders, discriptions, discussions, or
+# other statements to describe WHAT we did and WHY we did it
+# as well as report anything interesting
+
+
+"""
+This is a multi line or BLOCK comment
+
+Everything between the 3 double quotes are NOT evaluated 
+
+2+2
+
+2+5
+
+3*3
+
+6/3
+
+x=3
+
+y=5
+"""
+
+
+
